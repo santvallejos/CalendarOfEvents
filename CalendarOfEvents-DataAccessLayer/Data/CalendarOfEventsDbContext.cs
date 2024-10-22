fa88996ccf199d5ffcf18cb28b7ba6c1024a06df
santvallejos <vallejossantiago1412@gmail.com>
2024-10-21 22:44:07 -0300

    Creacion de repositorio

---
+++ CalendarOfEvents-DataAccessLayer/Data/CalendarOfEventsDbContext.cs
@@ -0,0 +1,33 @@
+﻿using CalendarOfEvents_DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarOfEvents_DataAccessLayer.Data
+{
+    public class CalendarOfEventsDbContext : DbContext
+    {
+        public CalendarOfEventsDbContext(DbContextOptions<CalendarOfEventsDbContext> options) : base(options)
+        {
+        }
+
+        //Entidades
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        //Modelo a crear
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //Configuracion de la base de datos
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ CalendarOfEvents-DataAccessLayer/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarOfEvents_DataAccessLayer.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public bool SendNotification { get; set; }
+    }
+}
